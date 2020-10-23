31e0031260d70e776be94a4558ddbd44042f34b9
kudzai.macheyo <kudzai.macheyo@cassavasmartech.co.zw>
2020-10-23 07:11:17 +0200

    Initial commit

---
+++ src/test/java/com/footprint/omsAPIgateway/OmsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footprint.omsAPIgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
